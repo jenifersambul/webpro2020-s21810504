5b9d78c71b24fbd996f1f3e05b822c1a4839a746
jenifersambul <63536411+jenifersambul@users.noreply.github.com>
2020-11-12 21:31:33 +0800

    React-People

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+// import Card from './Components/card';
+// import {person} from './Utils/person';
+// import Counter from "./Components/counter" 
 
-function App() {
+// function App() {
+//   return (
+//     <>
+//       {/*person.map((item) =>{
+//         return(
+//           <Card
+//             name={item.name}
+//             institution={item.institution}
+//             address={item.address}
+//             phone={item.phone}
+//           />
+//         );
+//       })*/}
+//       <Counter title = "Counter Component"/>
+//     </>
+//   );
+// }
+
+// export default App;
+
+import React from 'react';
+import './App.css';
+const sortByPower = (a, b) => {
+  return b.power - a.power;
+}
+const List = props => {
+  return (<>
+    <h1>React People!</h1>
+      <ul className="list">
+        {props.data.sort(sortByPower).map((person) => (
+          <li key={person.name} className="list-item">
+            <div className="list-item-image-container">
+              <img src={person.thumbnail} />
+            </div>
+            <span className="list-item-name">
+              {person.name} ( Power:{' '}
+            <span className="list-item-power">{person.power}</span> )
+          </span>
+        </li>
+      ))}
+    </ul>
+  </>
+  );
+}
+const App = () => {
+  const human = [
+    {
+      name: 'Krillin',
+      thumbnail:'https://upload.wikimedia.org/wikipedia/pt/6/63/Kuririn_42311.png',
+      power: 10
+    },  
+    {
+      name: 'Bulma',
+      thumbnail: 'https://upload.wikimedia.org/wikipedia/hu/1/1c/Bulma.png',
+      power: 3
+    },
+    {
+      name: 'Chi-chi',
+      thumbnail:'https://upload.wikimedia.org/wikipedia/hu/8/84/Chi-chi_dragon_ball_anime.jpg',
+      power: 30
+    }
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <List data={human} />
     </div>
   );
 }
-
 export default App;
---
+++ my-app/src/Utils/person.js
@@ -0,0 +1,26 @@
+export const person = [
+    {
+        name: "Jenifer Sambul",
+        institution: "Unklab",
+        address: "Manado",
+        phone: "085395646552",
+    },
+    {
+        name: "Park Jimin",
+        institution: "KAHS",
+        address: "Korea",
+        phone: "023xxxxxxx",
+    },
+    {
+        name: "Louis Partrige",
+        institution: "High School",
+        address: "Inggris",
+        phone: "083xxxxxxx",
+    },
+    {
+        name: "Septian Nugroho",
+        institution: "Ravispa",
+        address: "Jakarta",
+        phone: "082xxxxxxx",
+    },
+    ];
